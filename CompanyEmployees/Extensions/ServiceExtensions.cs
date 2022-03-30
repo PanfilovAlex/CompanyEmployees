a5ef8f545b5d99c68dd757b8af5631e6396d69ab
Alexander Panfilov <cosinusr@yahoo.com>
2022-03-30 11:16:09 +0300

    Добавьте файлы проекта.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection service) =>
+            service.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntedgration(this IServiceCollection service) =>
+            service.Configure<IISOptions>(options => {
+                
+            });
+
+
+    }
+}
