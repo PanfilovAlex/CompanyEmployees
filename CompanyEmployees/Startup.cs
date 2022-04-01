406e50e152adbd28628ffa945cd24a9a2d9d79c9
Alexander Panfilov <cosinusr@yahoo.com>
2022-04-01 11:32:19 +0300

    Удаление лишнего

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -1,16 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+using System.IO;
+
 
 namespace CompanyEmployees
 {
