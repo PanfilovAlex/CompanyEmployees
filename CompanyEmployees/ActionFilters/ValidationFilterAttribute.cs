6bfdec1bd4cf92fa7a5c06c43f8e658f869eaced
Alexander Panfilov <cosinusr@yahoo.com>
2022-04-08 11:14:10 +0300

    Начало работы с фильтрами

---
+++ CompanyEmployees/ActionFilters/ValidationFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CompanyEmployees.ActionFilters
+{
+    public class ValidationFilterAttribute : IActionFilter
+    {
+        private ILoggerManager _logger;
+
+        public ValidationFilterAttribute(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
